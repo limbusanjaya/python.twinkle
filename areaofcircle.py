e3f47c8b974d9e6c8af5f7b4619cf56b45de181c
limbusanjaya <3118620@student.bcot.ac.uk>
2025-01-28 11:19:15 +0000

    almightypush

---
+++ areaofcircle.py
@@ -0,0 +1,6 @@
+radius = input("Please enter the radius of a circle")
+def areaofcircle(radius):
+    areaofcircle = (22/7) * radius**2
+    print=("Area of circle is" + areaofcircle)
+
+def areaofcircle()
---
+++ expansioncalculator.py
@@ -0,0 +1,7 @@
+a = input("Please enter integer")
+def calculate(a):
+    calculate = a + aa + aaa
+    print("sum will be" + calculate)
+
+def calculate()
+
